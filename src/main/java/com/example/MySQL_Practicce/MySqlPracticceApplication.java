855940e2f1163be24e8603c214b465bacbb2aaaf
Kanghee Han <hcsy1219@gmail.com>
2023-05-23 23:51:37 +0900

    Create project

---
+++ src/main/java/com/example/MySQL_Practicce/MySqlPracticceApplication.java
@@ -0,0 +1,13 @@
+package com.example.MySQL_Practicce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlPracticceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqlPracticceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MySQL_Practicce/MySqlPracticceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MySQL_Practicce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqlPracticceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
