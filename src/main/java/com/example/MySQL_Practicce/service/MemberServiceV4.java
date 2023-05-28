73a101f50db9d04a8db4b50dd0337f4f7f0993b1
han <hcsy1219@gmail.com>
2023-05-28 16:52:53 +0900

    Transaction AOP

--- src/main/java/com/example/MySQL_Practicce/service/MemberServiceV4.java
+++ src/main/java/com/example/MySQL_Practicce/service/MemberServiceV4.java
@@ -2,35 +2,27 @@ package com.example.MySQL_Practicce.service;
 
 import com.example.MySQL_Practicce.domain.Member;
 import com.example.MySQL_Practicce.repository.MemberRepositoryV4;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 @Slf4j
-@RequiredArgsConstructor
 public class MemberServiceV4 {
 
     //private final PlatformTransactionManager transactionManager;
-    private final TransactionTemplate template;
+    //private final TransactionTemplate template;
     private final MemberRepositoryV4 memberRepository;
 
-    public MemberServiceV4(PlatformTransactionManager transactionManager, MemberRepositoryV4 memberRepository){
-        this.template = new TransactionTemplate(transactionManager);
+
+    public MemberServiceV4(MemberRepositoryV4 memberRepository){
         this.memberRepository = memberRepository;
     }
 
+    @Transactional
     public void accountTransfer(String fromId,String toId,int money) throws SQLException {
-        template.executeWithoutResult((status) ->{
-            try {
-                logic(fromId, toId, money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
+        logic(fromId, toId, money);
     }
 
     private void logic(String fromId, String toId, int money) throws SQLException {
@@ -42,6 +34,7 @@ public class MemberServiceV4 {
         memberRepository.update(toId, toMember.getMoney() + money);
     }
 
+    /*
     private static void release(Connection con) {
         if(con !=null){
             try {
@@ -52,6 +45,7 @@ public class MemberServiceV4 {
             }
         }
     }
+    */
 
     private static void validation(Member toMember) {
         if(toMember.getMemberId().equals("ex")){
--- src/test/java/com/example/MySQL_Practicce/service/MemberServiceTestV4.java
+++ src/test/java/com/example/MySQL_Practicce/service/MemberServiceTestV4.java
@@ -2,34 +2,71 @@ package com.example.MySQL_Practicce.service;
 
 import com.example.MySQL_Practicce.domain.Member;
 import com.example.MySQL_Practicce.repository.MemberRepositoryV4;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static com.example.MySQL_Practicce.connection.ConnectionSet.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Slf4j
+@SpringBootTest
 class MemberServiceTestV4 {
 
     public static final String member1="member1";
     public static final String member2="member2";
     public static final String exception="ex";
 
+    @Autowired
     private MemberRepositoryV4 memberRepository;
+    @Autowired
     private MemberServiceV4 memberService;
 
+    @TestConfiguration
+    static class TestConfig{
+        @Bean
+        DataSource dataSource(){
+            return new DriverManagerDataSource(URL, USERNAME,PASSWORD);
+        }
+
+        @Bean
+        PlatformTransactionManager transactionManager(){
+            return new DataSourceTransactionManager(dataSource());
+        }
+
+        @Bean
+        MemberRepositoryV4 memberRepositoryV4(){
+            return new MemberRepositoryV4(dataSource());
+        }
+
+        @Bean
+        MemberServiceV4 memberServiceV4(){
+            return new MemberServiceV4(memberRepositoryV4());
+        }
+    }
+
+    /*
     @BeforeEach
     void before(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME,PASSWORD);
         memberRepository=new MemberRepositoryV4(dataSource);
-        memberService = new MemberServiceV4(new DataSourceTransactionManager(dataSource), memberRepository);
+        memberService = new MemberServiceV4(memberRepository);
     }
+    */
 
     @AfterEach
     void after() throws SQLException {
@@ -37,6 +74,14 @@ class MemberServiceTestV4 {
         memberRepository.delete(member2);
         memberRepository.delete(exception);
     }
+
+    @Test
+    void AopCheck(){
+        log.info("memberService class={}",memberService.getClass());
+        log.info("memberRepository class={}",memberRepository.getClass());
+        Assertions.assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+        Assertions.assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
+    } //proxy
 
     @Test
     @DisplayName("normal operation")
